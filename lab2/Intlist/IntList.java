b8959769397855577ad77ad2c2a4ec99e8f278ce
a906842455 <46662317+a906842455@users.noreply.github.com>
2020-07-04 18:00:35 -0700

    update

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,9 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (B == null) {
+            return A;
+        }
         IntList p = A.rest;
         while (p.rest != null){
             p = p.rest;
