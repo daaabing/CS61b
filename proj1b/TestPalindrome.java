ef4eaad6e970748f185af1a63cf1a9259ca36574
han <hzwang@ucdavis.edu>
2020-12-28 23:26:39 -0800

    add

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -2,9 +2,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
+    // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
+    static CharacterComparator offByOne = new OffByOne();
 
     @Test
     public void testWordToDeque() {
@@ -14,5 +15,23 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    }
+
+    @Test
+    public void testPalindrome(){
+        assertFalse(palindrome.isPalindrome("cat"));
+    }
+
+    @Test
+    public void testOboPalindrome() {
+
+        assertTrue(palindrome.isPalindrome("flake", offByOne));
+        assertTrue(palindrome.isPalindrome("a", offByOne));
+
+        assertFalse(palindrome.isPalindrome("base", offByOne));
+        assertFalse(palindrome.isPalindrome("baseball", offByOne));
+        assertFalse(palindrome.isPalindrome("aba", offByOne));
+
+    }
+
 }
